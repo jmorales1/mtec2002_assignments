86e1635413169410ced36e007f3a86afe2946bb3
jmorales <jmorales@citytech.cuny.edu>
2012-02-29 20:31:01 -0500

    msg

---
+++ class5/three_letter_words.py
@@ -0,0 +1,22 @@
+"""
+Three Letter Words
+=====
+* Create a list of strings called words that contains "dog","lizard","cat","hawk","pig", and "ibek" 
+* Create an empty list called three_letter_words
+* Use a for loop to iterate through your words list...
+* If the length of your word is equal to 3, append it to your three_letter_words list
+* Print "Words: [your words list]"
+* Print "Three letter words: [your three_letter_words list]"
+
+Example Output:
+Words: ['dog', 'lizard', 'cat', 'hawk', 'pig', 'ibek']
+Three letter words: ['dog', 'cat', 'pig']
+"""
+
+words = (["Dog", "Lizard", "Cat", "Hawk", "Pig", "Ibek"])
+three_letter_words = []
+for x in words:
+	if len(x) == 3:
+		three_letter_words.append(x)
+print words
+print three_letter_words
