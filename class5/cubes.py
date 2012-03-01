16dcd49eb7d5dcf45d819305ed4e634ba61c1b9f
jmorales <jmorales@citytech.cuny.edu>
2012-02-29 19:50:11 -0500

    msg

---
+++ class5/cubes.py
@@ -0,0 +1,20 @@
+"""
+Cubes
+=====
+* Create a list of ints using range, or create a list of ints using literals (range(10) or [1,2,3])... assign it to a variable named numbers
+* Create an empty list called cubes
+* Iterate through the numbers list using a for loop
+* For every number, cube it and append it to the cubes list
+* Print out "Numbers: [your numbers list]"
+* Print out "Cubes: [your cubes list]"
+
+Example Output:
+Numbers: [0, 1, 2, 3]
+Cubes: [0, 1, 8, 27]
+"""
+
+numbers = range(10)
+cubes = []
+for a in numbers:
+	cubes.append(a**3)
+print cubes
